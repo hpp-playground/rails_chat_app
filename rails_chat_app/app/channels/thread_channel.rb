befa14fb58ccee3652963ad6736636010233eac2
塚越駿 <tsukakoshihayato@hpp.local>
2019-12-03 01:39:46 +0900

    chat機能の追加🔥

--- rails_chat_app/app/channels/thread_channel.rb
+++ rails_chat_app/app/channels/thread_channel.rb
@@ -1,12 +1,14 @@
 class ThreadChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "thread_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak
+  def speak(data)
+    # ßActionCable.server.broadcast 'thread_channel', message: data['message']
+    Message.create! content: data['message']
   end
 end
